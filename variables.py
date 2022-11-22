b3b8fd1e7b447c14c8b06d66e558e86c868cc07d
Ruben The Fixer <118835724+RubenTheFixer@users.noreply.github.com>
2022-11-22 16:53:56 +0100

    Primeraa carga
    Estoy subiendo mi primer portofolio

---
+++ variables.py
@@ -0,0 +1,98 @@
+""" a = 75
+b = 8
+if( a<b ):
+    print("a es menos que b")
+else:
+    print("b es menor que a")    
+    
+     """
+    
+"""     
+a = int(input("dime un numero"))
+if( (a >= 1) and (a <= 10)):
+    print("este numero esta esntre el 1 y el 10")
+elif((a >= 11)and (a<=20)):
+    print("Este numero esta entre el 11 y 20")
+elif((a >= 21)and(a <= 30)):
+    print("Este numero esta entr el 21 y el 30")     
+else:
+    print("Este numero NOOOO esta entre el 31 y infinito")    """
+    
+""" i = 1
+while(i <= 100):
+    print(i)
+    i+=1  
+print("has llegado al 100")            """
+
+""" for i in range(1, 101,2):
+    print(i)
+print("Has llegado al 100")     """
+
+""" 
+for i in "Hola Mundo" :
+    print(i)
+print(i)    
+ """
+"""  
+for i in range(2, 101,2):
+     print(i)
+print("has llegado al 100 en numeros pares")      """   
+
+"""   
+rango = list(range(10))
+print(rango)   """
+
+""" rango1= list(range(11)) 
+rango2= list(range(15,21))
+rango= rango1 + rango2
+print(rango)
+ """
+
+""" 
+rango= list(range(0,len("hola mundooooooooooooo")))
+print(rango)
+ """
+""" lista = []
+for i in range(10):
+"""   """
+    print(lista[i]) """
+    
+""" lista = []
+suma = 0
+media = 0
+for i in range (10):
+    numero = int(input("Dame un numero"))
+    lista.append(numero)
+    suma += numero
+media  = suma / len(lista) 
+for i in lista:
+    print(i)   
+
+print("La suma es ",suma)
+print("La media es" ,media)    
+
+
+
+
+"""  """
+number1 = 20
+number2 = 30
+sum = number1 + number2
+
+product = number1 * number2
+if product <= 1000:
+    print(product)  
+else :
+          print("Given 1 is "sum)
+
+number3 = 40
+number4 = 30 
+ 
+product = number3 * number4
+if product <= 1000:
+    print(product)  
+else :
+          print(sum)          """
+
+2  + 3 
+          
